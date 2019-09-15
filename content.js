7f186cc728e517c3af3244e447e225ee4557230c
arahansa <arahansa@naver.com>
2019-09-15 23:41:44 +0900

    불필요한 로그 없앰

--- content.js
+++ content.js
@@ -35,8 +35,6 @@ function handlePaging(e) {
 }
 
 chrome.storage.sync.get(['on', 'className', 'firstRow', 'lastRow', 'column'], function (items) {
-    console.log('items :', items)
-
     if (items.on) {
         $(document).on('keypress', handlePaging);
 
@@ -45,5 +43,7 @@ chrome.storage.sync.get(['on', 'className', 'firstRow', 'lastRow', 'column'], fu
         const firstRowVal = $("." + items.className).find(tr).eq(items.firstRow).find("td").eq(items.column).text()
         const lastRowVal = $("." + items.className).find(tr).eq(count - (items.lastRow)).find("td").eq(items.column).text()
         console.log('첫번째 줄 값 :', firstRowVal, ' 마지막 줄 값 : ', lastRowVal, '현재 페이지 :', getParameterByName('page'))
+    }else{
+        console.log('items :', items)
     }
 });
